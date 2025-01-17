fc64fe4a601160eebb7f7a10d6bfd8dab5b0891f
Gabriel Da Silva <gabrieldasilva8542@gmail.com>
2025-01-17 14:51:46 +0100

    ssl eliminated v1.1

--- src/config/db.ts
+++ src/config/db.ts
@@ -6,7 +6,11 @@ dotenv.config()
 const db = new Sequelize(process.env.DATABASE_URL!, {
     models: [__dirname + '/../models/**/*'],
     logging: false,
-
+    dialectOptions: {
+        ssl: {
+            require: 'true'
+        }
+    }
 }) 
 
 export default db
